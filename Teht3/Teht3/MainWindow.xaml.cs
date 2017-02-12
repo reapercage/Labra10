903676bc63349ce5b8f9e5b00f8d0f2892fa7413
reapercage <h4026@student.jamk.fi>
2017-02-12 05:33:13 +0200

    Teht3 valmis, mutta virheellisen syötteen käsittely puuttuu.

--- Teht3/Teht3/MainWindow.xaml.cs
+++ Teht3/Teht3/MainWindow.xaml.cs
@@ -25,145 +25,134 @@ namespace Teht3
             InitializeComponent();
         }
 
+        //muuttujat
         Random rnd = new Random();
         int[,] lot;
-        bool[] lotTruth = new bool[40];
-        int[] vikingLotto = new int[6];
-        bool[] vikingTruth = new bool[49];
-        int[] europot = new int[7];
-        bool[] euroTruth = new bool[51];
+        bool[] lotTruth;
         int x;
-        bool newText = false;
-        int rowcount;
         int draws;
+        int lotteryNumbers;
+        int rownumbers;
+        int rownumbersMinus;
+        int rowcount;
 
-        List<int[]> lottoRows = new List<int[]>();
-        //tilalle 2. ulotteinen taulukko
+        int parInt;
+        bool isNumber = false;
+        int n;
+        string howManyLetters;
+        char[] checkArray;
 
         private void btnDraw_Click(object sender, RoutedEventArgs e)
         {
-            draws = System.Convert.ToInt32(txtDraws.Text);
-            lot = new int[draws,7];
-            //for rivit
-            //if lotto, numerot 7, 40:stä
-            if (lottoItem.IsSelected == true)
+            if (emptyItem.IsSelected)
             {
-                //for draws
-                for(int z = 0; z < draws; z++)
+
+            }
+            else
+            {
+                //arvot pelin mukaan
+                if (lottoItem.IsSelected)
                 {
-                    for (int i = 0; i < 7; i++)
+                    lotTruth = new bool[40];
+                    lotteryNumbers = 40;
+                    rownumbers = 7;
+                    //txbRows.Text = "Lotto\n";
+                }
+                else if (vikingItem.IsSelected)
+                {
+                    lotTruth = new bool[49];
+                    lotteryNumbers = 49;
+                    rownumbers = 6;
+                    //txbRows.Text = "Viking Lotto\n";
+                }
+                else if (euroItem.IsSelected)
+                {
+                    lotTruth = new bool[51];
+                    lotteryNumbers = 51;
+                    rownumbers = 7;
+                    //txbRows.Text = "Eurojackpot\n";
+                }
+
+                //arvotaan numerot
+                if (txtDraws.Text == "")
+                {
+                    draws = 0;
+                }
+                //else if() sisältää muuta kuin kokonaisluvun
+                else
+                {
+                    //draws = Int32.Parse(txbRows.Text);
+                }
+                //kaatuu jos null
+                draws = System.Convert.ToInt32(txtDraws.Text);
+                lot = new int[draws, rownumbers];
+
+                for (int z = 0; z < draws; z++)
+                {
+                    for (int i = 0; i < rownumbers; i++)
                     {
-                        x = rnd.Next(1, 40);
+                        x = rnd.Next(1, lotteryNumbers);
                         while (lotTruth[x] == true)
                         {
-                            x = rnd.Next(1, 40);
+                            x = rnd.Next(1, lotteryNumbers);
                         }
-                        lot[z,i] = x;
+                        lot[z, i] = x;
                         lotTruth[x] = true;
                     }
-                    for (int i = 0; i < 40; i++)
+                    for (int i = 0; i < lotteryNumbers; i++)
                     {
                         lotTruth[i] = false;
                     }
-                    //lottoRows.Add(lot);
                 }
-            }
-            if (vikingItem.IsSelected == true)
-            {
-                for (int i = 0; i < 6; i++)
-                {
-                    x = rnd.Next(1, 49);
-                    while (vikingTruth[x] == true)
-                    {
-                        x = rnd.Next(1, 49);
-                    }
-                    vikingLotto[i] = x;
-                    vikingTruth[x] = true;
-                }
-                for (int i = 0; i < 49; i++)
-                {
-                    vikingTruth[i] = false;
-                }
-            }
-            if (euroItem.IsSelected == true)
-            {
-                for (int i = 0; i < 7; i++)
-                {
-                    x = rnd.Next(1, 51);
-                    while (euroTruth[x] == true)
-                    {
-                        x = rnd.Next(1, 51);
-                    }
-                    europot[i] = x;
-                    euroTruth[x] = true;
-                }
-                for (int i = 0; i < 51; i++)
-                {
-                    euroTruth[i] = false;
-                }
-            }
 
-            newText = true;
-            rowcount = 1;
-            foreach(int[] row in lottoRows)
-            {
-                if(newText == true)
+                //tulostus
+                rowcount = 1;
+                rownumbersMinus = rownumbers - 1;
+                txbRows.Text = "";
+                for (int i = 0; i < draws; i++)
                 {
-                    //txbRows.Text = "Row " + rowcount + ": ";
-                }
-                else
-                {
-                    //txbRows.Text = txbRows.Text + "\nRow " + rowcount + ": ";
-                }
-                foreach (int number in row)
-                {
-                    //txbRows.Text = txbRows.Text + number + ", ";
-                }
-                newText = false;
-                rowcount++;
-            }
-
-            rowcount = 1;
-            //vaihtoehtoinen tulostus
-            for(int i = 0; i < draws; i++)
-            {
-                if (i == 0)
-                {
-                    txbRows.Text = "";
-                }
-                for (int a = 0; a < 7; a++)
-                {
-                    if(a == 0)
+                    for (int a = 0; a < rownumbers; a++)
                     {
-                        txbRows.Text = txbRows.Text + "Row " + rowcount + ": ";
+                        if (a == 0)
+                        {
+                            txbRows.Text = txbRows.Text + "Row " + rowcount + ": ";
+                        }
+                        if (euroItem.IsSelected)
+                        {
+                            if (a == 5)
+                            {
+                                txbRows.Text = txbRows.Text + " + ";
+                            }
+                        }
+                        txbRows.Text = txbRows.Text + lot[i, a].ToString();
+                        if (a < rownumbersMinus)
+                        {
+                            if (euroItem.IsSelected && a == 4)
+                            {
+                            }
+                            else
+                            {
+                                txbRows.Text = txbRows.Text + ", ";
+                            }
+                        }
+                        else if (a == rownumbersMinus)
+                        {
+                            if(i != draws-1)
+                            {
+                                txbRows.Text = txbRows.Text + "\n";
+                            }
+                        }
                     }
-                    txbRows.Text = txbRows.Text + lot[i,a].ToString();
-                    if(a < 6)
-                    {
-                        txbRows.Text = txbRows.Text + ", ";
-                    }
-                    else if(a == 6)
-                    {
-                        txbRows.Text = txbRows.Text + "\n";
-                    }
+                    rowcount++;
                 }
-                rowcount++;
             }
             
-            
-            //vanha tulostus
-            for(int i = 0; i < 7; i++)
-            {
-                if(i == 0)
-                {
-                    //txbRows.Text = lot[i].ToString();
-                }
-                else
-                {
-                    //txbRows.Text = txbRows.Text + ", " + lot[i].ToString();
-                }
-            }
+        }
 
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txbRows.Text = "";
+            txtDraws.Text = "";
         }
     }
 }
